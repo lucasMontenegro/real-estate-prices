fe3ac23d7d6c771ae35194a4c516693044b66684
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2020-08-26 19:42:08 -0300

    Added baseline styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'fontsource-roboto';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          App en construcción. Visitá nuestra {' '}
-          <a
-            className="App-link"
-            href="https://www.facebook.com/terrenos.charata"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            página de Facebook
-          </a>
-          .
-        </p>
-      </header>
-    </div>
+    <Fragment>
+      <CssBaseline />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            App en construcción. Visitá nuestra {' '}
+            <a
+              className="App-link"
+              href="https://www.facebook.com/terrenos.charata"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              página de Facebook
+            </a>
+            .
+          </p>
+        </header>
+      </div>
+    </Fragment>
   );
 }
 
