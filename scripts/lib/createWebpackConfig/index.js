ef88ecf5d9b745128feae0de196c6177d4d926aa
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2020-11-30 19:42:13 -0300

    Deleted unused config files.

--- scripts/lib/createWebpackConfig/index.js
+++ scripts/lib/createWebpackConfig/index.js
@@ -521,7 +521,8 @@ function webpackConfigFactory(cfg) {
       // the requesting resource.
       new ModuleNotFoundPlugin(cfg.packageDir),
       // Makes some environment variables available to the JS code, for example:
-      // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
+      // if (process.env.NODE_ENV === 'production') { ... }. See:
+      // `./getClientEnvironment.js`.
       // It is absolutely essential that NODE_ENV is set to production
       // during a production build.
       // Otherwise React will be compiled in the very slow development mode.
