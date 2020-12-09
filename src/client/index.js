db04a3f50dd97bce472e484f1758a3da2b706e31
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2020-12-09 11:32:27 -0300

    Move shortlinks to cra.link (https://github.com/facebook/create-react-app/pull/8957)

--- src/client/index.js
+++ src/client/index.js
@@ -19,5 +19,5 @@ model.run();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: https://cra.link/CRA-PWA
 serviceWorker.unregister();
